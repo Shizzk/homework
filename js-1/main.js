d8a2520ffb7d414615e4984a8c0a53cc29fc1908
Shizzk <166852579+Shizzk@users.noreply.github.com>
2024-05-25 23:38:35 +0400

    js-1

---
+++ js-1/main.js
@@ -0,0 +1,10 @@
+const myName = 'Лера';
+let myAge = 17;
+const myHobby = 'Guitar';
+let myFavoriteSinger = 'Andrey Pirokinesis'
+const myCatName = 'Lesya'
+
+console.log(myName, myAge)
+console.log(myHobby, 'Моё хобби')
+console.log(myFavoriteSinger, 'Мой любимый исполнитель')
+console.log(myCatName, 'Имя моей кошки')
