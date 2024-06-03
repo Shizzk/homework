37ea37b2f69adaa6dd2606a43cc22de554bce3ac
Shizzk <166852579+Shizzk@users.noreply.github.com>
2024-06-03 23:49:00 +0400

    js-79

---
+++ js-79/main.js
@@ -0,0 +1,18 @@
+// let myAge = 17
+// let result = null
+
+//     if (myAge >= 18) {
+//         result = 'Вы совершеннолетняя'
+//         console.log (result)
+//     }
+//     else {
+//         result = 'Вам ещё нет 18'
+//         console.log (result)
+//     }
+
+// -------------------------------------------------------------------------//
+
+let myAge = 17
+let result = null
+
+myAge >= 18 ? console.log ('Вы совершеннолетняя') : console.log ('Вам ещё нет 18')
