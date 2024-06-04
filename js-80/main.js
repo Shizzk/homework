a943ad636ec6829703f9bb2993ee403ea0bb3a0d
Shizzk <166852579+Shizzk@users.noreply.github.com>
2024-06-04 22:38:59 +0400

    js-80

---
+++ js-80/main.js
@@ -0,0 +1,26 @@
+// for (let i = 2; i < 12; i = i + 2) {
+//     console.log (i)
+// }
+
+
+// for (let i=10; i>=1; i = i-1) {
+//     console.log (i);
+// }
+
+
+// let i = 2
+// console.log(i)
+
+//     while (i < 12) {
+//     i = i + 2
+//     console.log (i)
+//     }
+
+
+let i = 10
+console.log(i)
+
+    while (i>=1) {
+        i = i-1
+        console.log (i)
+    }
